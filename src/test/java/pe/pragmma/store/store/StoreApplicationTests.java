eb591e476a6a13fe3d9f49b9f322a78012d90751
Izpo17F <ponce.1714.frp@gmail.com>
2025-02-10 21:40:46 -0500

    first commit

---
+++ src/test/java/pe/pragmma/store/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.pragmma.store.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
