f413e70ec1613c67e44a3063971c17bf35c4f2a3
Izpo17F <ponce.1714.frp@gmail.com>
2025-02-23 21:08:44 -0500

    featuring: Correcion del spring security y eliminacion de la clase Constanst dado que no se utilizara para la nueva estructura propuesta

--- src/main/java/pe/pragmma/store/store/config/SecurityConfig.java
+++ src/main/java/pe/pragmma/store/store/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +12,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pe.pragmma.store.store.exception.CustomAccessDeniedHandler;
+import pe.pragmma.store.store.exception.CustomBasicAuthenticationEntryPoint;
 import pe.pragmma.store.store.filter.JwtFilter;
 
 @Configuration
@@ -24,10 +25,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
-
-        /*http.addFilterBefore(new JwtValidatorFilter(), BasicAuthenticationFilter.class);
-        http.addFilterAfter(new JwtGeneratorFilter(), BasicAuthenticationFilter.class);
-*/
         http
             .authorizeHttpRequests(requests ->
                 requests.requestMatchers( "/swagger-ui/**").permitAll()
@@ -45,9 +42,9 @@ public class SecurityConfig {
                     .anyRequest().authenticated()
             );
             http.formLogin(AbstractHttpConfigurer::disable);
-            http.httpBasic(Customizer.withDefaults());
-           // http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
-            //http.exceptionHandling(e -> e.accessDeniedHandler(new CustomAccessDeniedHandler()));
+           // http.httpBasic(Customizer.withDefaults());
+            http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+            http.exceptionHandling(e -> e.accessDeniedHandler(new CustomAccessDeniedHandler()));
 
 
             http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
